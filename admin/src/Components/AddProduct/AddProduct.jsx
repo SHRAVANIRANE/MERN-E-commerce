451f31cc0ce57b3630b56a05e286093611c039ae
Shravani Rane <117579713+SHRAVANIRANE@users.noreply.github.com>
2024-10-26 18:35:33 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -28,7 +28,7 @@ const AddProduct = () => {
     let product = productDetails;
     let formData = new FormData();
     formData.append("product", image);
-    await fetch("http://localhost:4000/upload", {
+    await fetch("https://mern-e-commerce-backend-jkse.onrender.com/upload", {
       method: "POST",
       headers: { Accept: "application/json" },
       body: formData,
@@ -40,7 +40,7 @@ const AddProduct = () => {
     if (responseData.success) {
       product.image = responseData.image_url;
       console.log(product);
-      await fetch("http://localhost:4000/addproduct", {
+      await fetch("https://mern-e-commerce-backend-jkse.onrender.com/addproduct", {
         method: "POST",
         headers: {
           Accept: "application/json",
