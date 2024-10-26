828815bf6201310324afc021cc4de7fe60722508
Shravani Rane <117579713+SHRAVANIRANE@users.noreply.github.com>
2024-10-26 18:36:43 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -7,7 +7,7 @@ const ListProduct = () => {
 
   // Fetching data from the backend API
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch("https://mern-e-commerce-backend-jkse.onrender.com/allproducts")
       .then((res) => res.json())
       .then((data) => {
         setAllProducts(data);
@@ -22,7 +22,7 @@ const ListProduct = () => {
   }, []);
 
   const remove_product = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch("https://mern-e-commerce-backend-jkse.onrender.com/removeproduct", {
       method: "POST",
       headers: {
         Accept: "application/json",
